6f5b31ca23b9ffda7f4a88aff8cd0e8d8e38da7d
Neha Gulabani <gulabani.ng@gmail.com>
2025-01-16 19:00:09 +0800

    fixing deployment issues

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -122,7 +122,7 @@ function MainPage() {
 
             if (searchTerm === '') {
                 setSearchRes([]);
-                setReposList([]);
+
             }
         }
     }, [debouncedRes, searchTerm]);
